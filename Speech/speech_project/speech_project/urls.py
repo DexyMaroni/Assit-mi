0250cc8f7bdf54b8e897385123c980b9b43bce97
Dexy Maroni <divineinyang7@gmail.com>
2024-12-04 10:34:23 +0100

    Final Changes

--- Speech/speech_project/speech_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('speech_app.urls')),  # Include the app's URLs
-]
